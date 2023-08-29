c7b2c494ba984e1f7f09c1e41c6a35a6b247e706
Dani-dornas <lauby.dd@gmail.com>
2023-08-28 22:53:14 -0300

    28 de agosto pt5

--- 28 de agosto/servidor/src/index.ts
+++ 28 de agosto/servidor/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { sum, dif } from "./operacao";
+import { sum, dif } from "./controllers/operacao";
 import dotenv from "dotenv";
-import { dayOfMonth, month, year } from "./date";
+import { dayOfMonth, month, year } from "./controllers/date";
+import routes from "./routes";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
@@ -12,8 +13,8 @@ app.listen(PORT, function () {
   console.log(`Rodando ${PORT}`);
 });
 
-app.get("/somar/:x/:y", sum);
-app.post("/subtrair", dif);
-app.get("/dia", dayOfMonth);
-app.get("/mes", month);
-app.get("/ano", year);
+app.use(routes);
+
+app.use(function(req, res){
+  res.send("Rota desconhecida");
+});
