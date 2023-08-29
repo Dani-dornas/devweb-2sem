bff532684956051ebd1e58762194d0ccffc6494e
Dani-dornas <lauby.dd@gmail.com>
2023-08-28 22:06:36 -0300

    28 de agosto pt4

--- 28 de agosto/servidor/src/index.ts
+++ 28 de agosto/servidor/src/index.ts
@@ -1,22 +1,19 @@
 import express from "express";
-import {sum, dif} from "./operacao";
+import { sum, dif } from "./operacao";
 import dotenv from "dotenv";
+import { dayOfMonth, month, year } from "./date";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
-app.listen(PORT, function(){console.log(`Rodando ${PORT}`)});
-
-app.get("/somar/:w/:z", function (req, res) {
-  const {w,z} = req.params;
-  let r = parseFloat(w) + parseFloat(z);
-  res.send({r});
+app.listen(PORT, function () {
+  console.log(`Rodando ${PORT}`);
 });
 
-app.post("/subtrair", function (req, res) {
-  const {x,y} = req.body;
-  let r = parseFloat(x) - parseFloat(y);
-  res.send({r});
-});
+app.get("/somar/:x/:y", sum);
+app.post("/subtrair", dif);
+app.get("/dia", dayOfMonth);
+app.get("/mes", month);
+app.get("/ano", year);
